5925e9408c8c3c7de5d23ce3012b833a8e8408d2
carl2087 <carlelli5@hotmail.co.uk>
2023-07-30 19:35:34 +0100

    built directory component with styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
+import Header from './components/header';
 import './default.scss'
 import Homepage from './pages/Homepage';
-import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='main'>
       <Homepage />
+      </div>
     </div>
   );
 };
---
+++ src/components/Directory/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import ShopMen from '../../assets/serious-handsome-young-man-sitting-isolated.jpg'
+import ShopWomen from '../../assets/woman-with-shopping-bags-studio-yellow-background-isolated.jpg'
+import './styles.scss'
+
+const Directory = props => {
+    return (
+        <div className="directory">
+            <div className="wrap">
+                <div
+                    className='item'
+                    style={{
+                        backgroundImage: `url(${ShopWomen})`
+                    }}
+                >
+
+                    <a>
+                        Shop Womens
+                    </a>
+
+                </div>
+                <div
+                    className='item'
+                    style={{
+                        backgroundImage: `url(${ShopMen})`
+                    }}
+                >
+
+                    <a>
+                        Shop Mens
+                    </a>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Directory;
