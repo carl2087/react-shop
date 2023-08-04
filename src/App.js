4fc5f2b5048b09172a5baaadfe4a853db12dbbe9
carl2087 <carlelli5@hotmail.co.uk>
2023-08-04 16:58:55 +0100

    installed react router dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import Header from './components/header';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom';
 import './default.scss'
 import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <div className='main'>
-      <Homepage />
+        <Switch>
+          <Route exact path='/' component={Homepage} />
+          <Route exact path='/registration' component={Registration} />
+        </Switch>
       </div>
     </div>
   );
