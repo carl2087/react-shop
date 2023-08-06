792c3eb144d38364ca1d33aee3b1c369360f1878
carl2087 <carlelli5@hotmail.co.uk>
2023-08-06 16:48:41 +0100

    added google login

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import './styles.scss';
-import Logo from '../../assets/react-shop-logo.png'
-import { NavLink} from 'react-router-dom'
+import Logo from '../../assets/react-shop-logo.png';
+import { NavLink} from 'react-router-dom';
+import { auth } from '../../firebase/utils';
 
 const Header = props => {
+
+    const { currentUser } = props;
+
     return (
         <header className="header">
             <div className="wrap">
@@ -14,17 +18,40 @@ const Header = props => {
                 </div>
 
                 <div className="callToActions">
+
+                    {currentUser && (
+                        <ul>
+                            <li>
+                                <span onClick={() => auth.signOut()}>
+                                    LogOut
+                                </span>
+                            </li>
+                        </ul>
+                    )}
+
+                    {!currentUser && (
                     <ul>
                         <li>
                             <NavLink to='/registration'>
                                 Register
                             </NavLink>
                         </li>
+                        <li>
+                            <NavLink to='/login'>
+                                Login
+                            </NavLink>
+                        </li>
                     </ul>
+                    )}
+
                 </div>
             </div>
         </header>
     );
+};
+
+Header.defaultProps = {
+    currentUser: null
 };
 
 export default Header;
--- src/firebase/utils.js
+++ src/firebase/utils.js
@@ -1,4 +1,38 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth'
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 import { firebaseConfig } from './config';
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const GoogleProvider = new firebase.auth.GoogleAuthProvider();
+GoogleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(GoogleProvider);
+
+export const handleUserProfile = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const { uid } = userAuth;
+
+    const userRef = firestore.doc(`users/${uid}`);
+    const snapshot = await userRef.get();
+
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const timeStamp = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdDate: timeStamp,
+                ...additionalData
+            });
+        } catch (error) {
+            // console.log(error)
+        }
+    }
+    return userRef;
+};
