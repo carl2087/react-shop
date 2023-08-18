bf1c8409b36540a796da58d14ce46a6f73fdd159
carl2087 <carlelli5@hotmail.co.uk>
2023-08-18 15:28:22 +0100

    set up redux thorughout app

--- src/App.js
+++ src/App.js
@@ -1,98 +1,106 @@
-import React, { Component } from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import './default.scss'
-import { auth, handleUserProfile } from './firebase/utils';
-import { setCurrentUser } from './redux/User/user.actions';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import "./default.scss";
+import { auth, handleUserProfile } from "./firebase/utils";
+import { setCurrentUser } from "./redux/User/user.actions";
+import { connect } from "react-redux";
 
-//layouts 
-import MainLayout from './layouts/MainLayout';
-import HomepageLayout from './layouts/HomePageLayout';
+//layouts
+import MainLayout from "./layouts/MainLayout";
+import HomepageLayout from "./layouts/HomePageLayout";
 
 // pages
-import Homepage from './pages/Homepage';
-import Registration from './pages/Registration';
-import Login from './pages/Login';
-import Recovery from './pages/Recovery';
-
-const initialState = {
-  currentUser: null
-};
+import Homepage from "./pages/Homepage";
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import Recovery from "./pages/Recovery";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...initialState
-    };
-  }
-
   authListener = null;
 
   componentDidMount() {
-    this.authListener = auth.onAuthStateChanged(async userAuth => {
-        if (userAuth) {
-          const userRef = await handleUserProfile(userAuth);
-          userRef.onSnapshot(snapshot => {
-            this.setState({
-              currentUser: {
-                id: snapshot.id,
-                ...snapshot.data()
-              }
-            })
-          })
-        }
-        this.setState({
-          ...initialState
-        });
-        });
-      };
+    const { setCurrentUser } = this.props;
 
+    this.authListener = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await handleUserProfile(userAuth);
+        userRef.onSnapshot((snapshot) => {
+          setCurrentUser({
+            id: snapshot.id,
+            ...snapshot.data(),
+          });
+        });
+      }
+      setCurrentUser(userAuth);
+    });
+  }
 
   componentWillUnmount() {
     this.authListener();
   }
 
-  render () {
-
-    const { currentUser } = this.state;
+  render() {
+    const { currentUser } = this.props;
 
     return (
       <div className="App">
-          <Switch>
-            <Route exact path='/' render={() => (
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => (
               <HomepageLayout>
                 <Homepage />
               </HomepageLayout>
-            )}/>
-            <Route exact path='/registration' render={() => currentUser ? <Redirect to='/' /> : (
-              <MainLayout>
-                <Registration />
-              </MainLayout>
-            )}/>
-            <Route exact path='/login'
-              render={() => currentUser ? <Redirect to='/' /> : (
+            )}
+          />
+          <Route
+            exact
+            path="/registration"
+            render={() =>
+              currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <MainLayout>
+                  <Registration />
+                </MainLayout>
+              )
+            }
+          />
+          <Route
+            exact
+            path="/login"
+            render={() =>
+              currentUser ? (
+                <Redirect to="/" />
+              ) : (
                 <MainLayout>
                   <Login />
                 </MainLayout>
-              )}/>
-            <Route exact path='/recovery' render={() => (
+              )
+            }
+          />
+          <Route
+            exact
+            path="/recovery"
+            render={() => (
               <MainLayout>
                 <Recovery />
               </MainLayout>
-            )}/>
-          </Switch>
+            )}
+          />
+        </Switch>
       </div>
     );
   }
-};
+}
 
 const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+  currentUser: user.currentUser,
 });
 
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
